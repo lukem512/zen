434dfbd75b7bdd31bc6d1446fde6d231ae2cf54f
lukem512 <lukem95@gmail.com>
2016-02-25 10:17:39 +0000

    Remove redundant module

--- models/users.js
+++ models/users.js
@@ -1,5 +1,4 @@
 var mongoose = require('mongoose');
-var bcrypt = require('bcrypt-nodejs');
 
 var UserSchema = new mongoose.Schema({
   username: {
@@ -23,30 +22,5 @@ var UserSchema = new mongoose.Schema({
     default: false
   }
 });
-
-// Execute before each user.save() call
-UserSchema.pre('save', function(callback) {
-  var user = this;
-
-  if (!user.isModified('password')) return callback();
-
-  // Password changed so we need to hash it
-  bcrypt.genSalt(10, function(err, salt) {
-    if (err) return callback(err);
-
-    bcrypt.hash(user.password, salt, null, function(err, hash) {
-      if (err) return callback(err);
-      user.password = hash;
-      callback();
-    });
-  });
-});
-
-UserSchema.methods.verifyPassword = function(password, callback) {
-  bcrypt.compare(password, this.password, function(err, isMatch) {
-    if (err) return callback(err);
-    callback(null, isMatch);
-  });
-};
 
 module.exports = mongoose.model('User', UserSchema);
